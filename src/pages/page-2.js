d37ae5241213872f560af1dc0df705006509b947
Jonny Kalambay <jonnyk20@gmail.com>
2019-06-26 03:32:26 +0000

    Programatic

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -5,6 +5,31 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+function preventPullToRefresh(element) {
+  var prevent = false
+
+  document.querySelector(element).addEventListener("touchstart", function(e) {
+    if (e.touches.length !== 1) {
+      return
+    }
+
+    var scrollY =
+      window.pageYOffset ||
+      document.body.scrollTop ||
+      document.documentElement.scrollTop
+    prevent = scrollY === 0
+  })
+
+  document.querySelector(element).addEventListener("touchmove", function(e) {
+    if (prevent) {
+      prevent = false
+      e.preventDefault()
+    }
+  })
+}
+
+preventPullToRefresh("html") // pas
+
 const style = {
   display: "flex",
   alignItems: "center",
