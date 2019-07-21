4e8bb3f189ff927f880150cb71be1d7d03b858fa
Jonny Kalambay <jonnyk20@gmail.com>
2019-07-21 04:35:15 +0000

    no rotate

--- src/pages/fish-demo.jsx
+++ src/pages/fish-demo.jsx
@@ -125,40 +125,40 @@ const FishDemo = () => {
     const ctx = canvas.getContext("2d")
 
     // set proper canvas dimensions before transform & export
-    if (4 < orientation && orientation < 9) {
-      canvas.width = height
-      canvas.height = width
-    } else {
-      canvas.width = width
-      canvas.height = height
-    }
+    // if (4 < orientation && orientation < 9) {
+    //   canvas.width = height
+    //   canvas.height = width
+    // } else {
+    //   canvas.width = width
+    //   canvas.height = height
+    // }
 
     // transform context before drawing image
-    switch (orientation) {
-      case 2:
-        ctx.transform(-1, 0, 0, 1, width, 0)
-        break
-      case 3:
-        ctx.transform(-1, 0, 0, -1, width, height)
-        break
-      case 4:
-        ctx.transform(1, 0, 0, -1, 0, height)
-        break
-      case 5:
-        ctx.transform(0, 1, 1, 0, 0, 0)
-        break
-      case 6:
-        ctx.transform(0, 1, -1, 0, height, 0)
-        break
-      case 7:
-        ctx.transform(0, -1, -1, 0, height, width)
-        break
-      case 8:
-        ctx.transform(0, -1, 1, 0, 0, width)
-        break
-      default:
-        break
-    }
+    // switch (orientation) {
+    //   case 2:
+    //     ctx.transform(-1, 0, 0, 1, width, 0)
+    //     break
+    //   case 3:
+    //     ctx.transform(-1, 0, 0, -1, width, height)
+    //     break
+    //   case 4:
+    //     ctx.transform(1, 0, 0, -1, 0, height)
+    //     break
+    //   case 5:
+    //     ctx.transform(0, 1, 1, 0, 0, 0)
+    //     break
+    //   case 6:
+    //     ctx.transform(0, 1, -1, 0, height, 0)
+    //     break
+    //   case 7:
+    //     ctx.transform(0, -1, -1, 0, height, width)
+    //     break
+    //   case 8:
+    //     ctx.transform(0, -1, 1, 0, 0, width)
+    //     break
+    //   default:
+    //     break
+    // }
     drawResized(img, canvas, ctx)
   }
 
